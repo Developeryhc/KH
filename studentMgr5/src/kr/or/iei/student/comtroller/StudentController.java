38fae0e43076172cd6e8da34f6160e8b2d942235
Developeryhc <78771179+Developeryhc@users.noreply.github.com>
2021-03-04 16:26:42 +0900

    숙제겸 공부

---
+++ studentMgr5/src/kr/or/iei/student/comtroller/StudentController.java
@@ -0,0 +1,11 @@
+package kr.or.iei.student.comtroller;
+
+import java.util.HashMap;
+
+import kr.or.iei.student.vo.Student;
+
+public class StudentController {
+	// key값은 이름으로, value는 학생객체로 관리
+	HashMap<String, Student> students;
+	
+}
---
+++ studentMgr5/src/kr/or/iei/student/run/StudentRun.java
@@ -0,0 +1,10 @@
+package kr.or.iei.student.run;
+
+public class StudentRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
